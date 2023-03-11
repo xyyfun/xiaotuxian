5c495f684338ec1e4a658483252e25b2dee232db
Turmp <Turmp@126.com>
2023-03-11 16:30:46 +0800

    新增分类页排序功能

--- src/views/category/sub/components/sub-sort.vue
+++ src/views/category/sub/components/sub-sort.vue
@@ -1,16 +1,30 @@
 <template>
 	<div class="sub-sort">
 		<div class="sort">
-			<a class="active" href="javascript:;">默认排序</a><a class="" href="javascript:;">最新商品</a
-			><a class="" href="javascript:;">最高人气</a><a class="" href="javascript:;">评论最多</a
-			><a href="javascript:;"> 价格排序 <i class="arrow up"></i><i class="arrow down"></i></a>
+			<a
+				:class="{ active: now === item.sortMethod }"
+				href="javascript:;"
+				v-for="(item, index) in sort"
+				:key="index"
+				@click="changFilter(item.sortMethod)">
+				{{ item.name }}
+			</a>
+			<!-- <a class="" href="javascript:;">最新商品</a>
+			<a class="" href="javascript:;">最高人气</a>
+			<a class="" href="javascript:;">评论最多</a> -->
 		</div>
 		<div class="check">
-			<div class="xtx-checkbox">
-				<i class="iconfont icon-unchecked"></i><span>仅显示有货商品</span>
+			<div class="xtx-checkbox" @click="inventory" :class="available ? 'active' : ''">
+				<i
+					class="iconfont"
+					:class="available ? 'icon-duoxuanxuanzhong' : 'icon-duoxuanweixuanzhong'"></i>
+				<span>仅显示有货商品</span>
 			</div>
-			<div class="xtx-checkbox">
-				<i class="iconfont icon-unchecked"></i><span>仅显示特惠商品</span>
+			<div class="xtx-checkbox" @click="onlyDiscount" :class="discounts ? 'active' : ''">
+				<i
+					class="iconfont"
+					:class="discounts ? 'icon-duoxuanxuanzhong' : 'icon-duoxuanweixuanzhong'"></i>
+				<span>仅显示特惠商品</span>
 			</div>
 		</div>
 	</div>
@@ -19,6 +33,42 @@
 <script>
 export default {
 	name: 'SubSort',
+	data() {
+		return {
+			sort: [
+				{
+					sortMethod: 'default',
+					name: '默认排序',
+				},
+				{
+					sortMethod: 'asc',
+					name: '价格升序',
+				},
+				{
+					sortMethod: 'desc',
+					name: '价格降序',
+				},
+			],
+			now: 'default',
+			available: false, // 是否有货
+			discounts: false, // 是否特惠
+		};
+	},
+	methods: {
+		changFilter(sortMethod) {
+			if (sortMethod === this.now) return;
+			this.now = sortMethod;
+			this.$bus.$emit('changFilter', sortMethod);
+		},
+		inventory() {
+			this.available = !this.available;
+			this.$bus.$emit('changFilter', 'inventory', this.available);
+		},
+		onlyDiscount() {
+			this.discounts = !this.discounts;
+			this.$bus.$emit('changFilter', 'onlyDiscount', this.discounts);
+		},
+	},
 };
 </script>
 
@@ -38,7 +88,6 @@ export default {
 			margin-right: 20px;
 			color: #999;
 			border-radius: 2px;
-			position: relative;
 			transition: all 0.3s;
 		}
 		.active {
@@ -53,6 +102,7 @@ export default {
 			color: #999;
 			display: inline-block;
 			margin-right: 2px;
+			cursor: pointer;
 			i {
 				position: relative;
 				top: 1px;
@@ -60,6 +110,9 @@ export default {
 			span {
 				margin-left: 2px;
 			}
+		}
+		.active {
+			color: #27ba9b;
 		}
 	}
 }
